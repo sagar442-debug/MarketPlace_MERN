5f5062edc4801b6193ef2fb751383fa85d7bc858
sagar442-debug <bnradlyp1@gmail.com>
2024-04-30 08:34:31 -0600

    changing the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,52 +10,36 @@ import {
 const Footer = () => {
   return (
     <footer className="bg-[#2b2b2b] py-4 px-6 flex items-center justify-center space-x-80 font-monsterrat">
-      <div className="text-gray-500">
-        <div>
-          <h1 className="">Our MarketPlace</h1>
+      <div className="max-w-[1080px] text-sm md:text-xl flex items-center justify-center ">
+        <div className="text-[#8a8989] mr-10">
+          Thank you for visiting my website.<br></br> Checkout my socials here:
         </div>
-        <ul>
-          <li>Home</li>
-          <li>Contact</li>
-          <li>Profile</li>
-        </ul>
-      </div>
-      <div className="text-gray-500">
-        <div>
-          <h1 className="">Thank you!</h1>
+        <div className="flex items-center space-x-4">
+          <a
+            href="https://www.instagram.com/sagarsapkota091/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white hover:text-gray-400"
+          >
+            <FaInstagram className="text-3xl" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sagar-sapkota091/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white hover:text-gray-400"
+          >
+            <FaLinkedin className="text-3xl" />
+          </a>
+          <a
+            href="https://github.com/sagar442-debug"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white hover:text-gray-400"
+          >
+            <FaGithub className="text-3xl" />
+          </a>
         </div>
-        <ul>
-          <li>Random Products</li>
-          <li>Shirts</li>
-          <li>Pants</li>
-        </ul>
-      </div>
-
-      <div className="flex items-center space-x-4">
-        <a
-          href="https://www.instagram.com/sagarsapkota091/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-white hover:text-gray-400"
-        >
-          <FaInstagram className="text-3xl" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/sagar-sapkota091/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-white hover:text-gray-400"
-        >
-          <FaLinkedin className="text-3xl" />
-        </a>
-        <a
-          href="https://github.com/sagar442-debug"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-white hover:text-gray-400"
-        >
-          <FaGithub className="text-3xl" />
-        </a>
       </div>
     </footer>
   );
